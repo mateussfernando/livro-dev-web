813b6880580739f6b2eba2684eb6263726d38ca1
Mateus Fernando <mateusfss368@gmail.com>
2025-02-21 08:29:55 +0000

    primeira seção da landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-[4rem] md:mt-[9rem]">
+      <div>
+        <h1 className="text-black text-[25px] font-['Press_Start_2P'] font-normal leading-normal">
+          LER PARA CODAR MELHOR.
+        </h1>
+        <span className="text-black text-[32px] font-[Montserrat] font-normal leading-normal">
+          Descubra os livros essenciais para evoluir na programação e acelerar
+          sua carreira tech.
+        </span>
+      </div>
+
+      <div>
+        <Image src="/homemSentado.png" alt="Logo" width={411} height={411} />
+      </div>
+    </section>
   );
 }
