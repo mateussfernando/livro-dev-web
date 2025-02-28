3fc4e0bce10eadd84d6385bd3495de74e1e45b6f
Mateus Fernando <mateusfss368@gmail.com>
2025-02-28 07:07:13 +0000

    implementação da bottom nav bar mobile

---
+++ src/components/layout/bottomNavMobile.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export function BOTTOMNAVMOBILE() {
+  return (
+    <nav className="block sm:hidden md:hidden h-20 fixed bottom-0 left-0 w-full bg-white shadow-md p-2">
+      <ul className="flex justify-around">
+        <li className="flex flex-col items-center text-gray-600">
+          <div className="w-6 h-6 bg-gray-400"></div>
+          <span className="text-xs">HOME</span>
+        </li>
+        <li className="flex flex-col items-center text-gray-600">
+          <div className="w-6 h-6 bg-gray-400"></div>
+          <span className="text-xs">EXPLORAR</span>
+        </li>
+        <li className="flex flex-col items-center text-gray-600">
+          <div className="w-6 h-6 bg-gray-400"></div>
+          <span className="text-xs">SALVOS</span>
+        </li>
+        <li className="flex flex-col items-center text-gray-600">
+          <div className="w-6 h-6 bg-gray-400"></div>
+          <span className="text-xs">PERFIL</span>
+        </li>
+      </ul>
+    </nav>
+  );
+}
